24b15f1b91d440a857e2711d6bb902fac7302b4d
Mirko Schuerlein <mirkoschuerlein@googlemail.com>
2023-02-06 16:45:51 +0100

    changed db numbers to string

--- website/auth.py
+++ website/auth.py
@@ -52,8 +52,12 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, 
+                            username=username, 
+                            password=generate_password_hash(password1, method='sha256'),
+                            business_id="",
+                            access_token="",
+                            phone_number="")
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    phone_number = db.Column(db.Integer)
+    phone_number = db.Column(db.String(12))
     email = db.Column(db.String(150))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(150))
     username = db.Column(db.String(150))
-    business_id = db.Column(db.Integer)
+    business_id = db.Column(db.String(30))
     access_token = db.Column(db.String(300))
-    phone_number = db.Column(db.Integer)
+    phone_number = db.Column(db.String(30))
     contacts = db.relationship('Contact')
